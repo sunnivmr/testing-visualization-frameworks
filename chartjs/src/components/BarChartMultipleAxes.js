6b01cf036ef80beb51403a4e60cdc930488be403
sunnivmr <sunnivmr@stud.ntnu.no>
2020-09-29 12:29:23 +0200

    #1 Tested out multiple axes and responsitivity on charts

--- chartjs/src/components/BarChartMultipleAxes.js
+++ chartjs/src/components/BarChartMultipleAxes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 
-/* For testing BarCharts in Chart.js*/
-function BarChart() {
+/* For testing BarCharts with multiple axes in Chart.js*/
+function BarChartMultipleAxes() {
   const data = {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
     datasets: [
@@ -10,11 +10,15 @@ function BarChart() {
         label: "2019",
         data: [130, 100, 90, 200, 70, 25, 10, 48],
         backgroundColor: "rgba(255, 200, 100, 0.2)",
+        yAxisID: "first-y-axis",
+        xAxisID: "first-x-axis",
       },
       {
         label: "2020",
         data: [150, 125, 70, 150, 100, 30, 40, 75],
         backgroundColor: "rgba(235, 49, 170, 0.2)",
+        yAxisID: "second-y-axis",
+        xAxisID: "first-x-axis",
       },
     ],
   };
@@ -26,11 +30,26 @@ function BarChart() {
     scales: {
       yAxes: [
         {
+          id: "first-y-axis",
           ticks: {
             min: 0,
-            max: 250,
+            max: 350,
             stepSize: 50,
           },
+        },
+        {
+          id: "second-y-axis",
+          ticks: {
+            min: 0,
+            max: 350,
+            stepSize: 25,
+          },
+          position: "right",
+        },
+      ],
+      xAxes: [
+        {
+          id: "first-x-axis",
         },
       ],
     },
@@ -43,4 +62,4 @@ function BarChart() {
   );
 }
 
-export default BarChart;
+export default BarChartMultipleAxes;
